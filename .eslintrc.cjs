fbe2480b134bf8ef2d7add855325930f49083af6
luoying2 <luoying2@37.com>
2023-02-14 11:58:40 +0800

    update

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  overrides: [
+    {
+      files: ["*.json", "*.json5", "*.jsonc"],
+      parser: "jsonc-eslint-parser",
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "plugin:jsonc/recommended-with-jsonc",
+    "plugin:markdown/recommended",
+    "prettier",
+  ],
+  rules: {
+    // prettier
+    "prettier/prettier": "error",
+  },
+};
