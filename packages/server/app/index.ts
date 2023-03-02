8cade78955cbdd9f52f29441e60ea1ec9cd2cd59
luoying <342231335@qq.com>
2023-03-02 23:49:41 +0800

    更新ts配置

--- packages/server/app/index.ts
+++ packages/server/app/index.ts
@@ -1,20 +1,18 @@
-'use strict';
-
 import Koa from 'koa';
 import router from './routes';
 import cors from 'koa2-cors';
 import bodyParser from 'koa-bodyparser';
-import middlewares from './middlewares';
+// import middlewares from './middlewares';
 import helmet from 'koa-helmet';
 const app = new Koa();
 
 app
     .use(helmet())
-    .use(middlewares())
+//     .use(middlewares())
     .use(bodyParser())
     .use(cors())
-    .use(router.routes())
-    .use(router.allowedMethods()); 
+    // .use(router.routes())
+    // .use(router.allowedMethods()); 
 
-app.listen(8001);
+app.listen(8000);
 export default app;
