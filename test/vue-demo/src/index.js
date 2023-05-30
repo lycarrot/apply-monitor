28b42ed6a28fb47607d27e4ed54d2b52c77d38ea
luoying <342231335@qq.com>
2023-05-30 12:19:15 +0800

    feat: 新增白屏检测

--- test/vue-demo/src/index.js
+++ test/vue-demo/src/index.js
@@ -1,22 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router  from './router';
-import Monitor from '@apply-monitor/monitor';
-
+import router from './router'
+import Monitor from '@apply-monitor/monitor'
 
 new Monitor({
-    url: 'http://localhost:8080/api/collect/info/detail',
-    project:'testvue',
-    version:'1.0.2',
-    isVue: true,
-    vue: Vue,
-    isCollectPer: false,
+  url: 'http://localhost:8080/api/collect/info/detail',
+  project: 'testvue',
+  version: '1.0.2',
+  isVue: true,
+  vue: Vue,
+  // isCollectPer: false,
 })
-
 
 new Vue({
-    el: '#app',
-    router,
-    render: (h) => h(App),
+  // el: '#app',
+  router,
+  render: (h) => h(App),
 })
-
