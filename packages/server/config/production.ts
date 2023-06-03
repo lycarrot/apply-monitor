1c1ed81bbd7518e2c87f95492bab5648feca6f87
luoying <342231335@qq.com>
2023-06-03 12:42:05 +0800

    feat: 修改配置文件

---
+++ packages/server/config/production.ts
@@ -0,0 +1,21 @@
+export default {
+  port: 80,
+  host: '8.134.132.121',
+  secret: '123456',
+  mysql: {
+    database: 'monitor-release',
+    user: 'root',
+    password: '123456',
+    host: '8.134.132.121',
+    port: 3306,
+  },
+  redis: {
+    host: '8.134.132.121',
+    port: 6379,
+  },
+  kueRedis: {
+    host: '8.134.132.121',
+    port: 6379,
+  },
+  sourceMap: 'static/sourcemap',
+}
