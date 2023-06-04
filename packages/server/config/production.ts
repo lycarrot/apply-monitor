4584579b472b5c8a24a1d86cdb6d7632cd148978
luoying <342231335@qq.com>
2023-06-04 17:25:20 +0800

    feat: 修改服务端配置

--- packages/server/config/production.ts
+++ packages/server/config/production.ts
@@ -6,15 +6,15 @@ export default {
     database: 'monitor',
     user: 'root',
     password: '123456',
-    host: '8.134.132.121',
+    host: '127.0.0.1',
     port: 3306,
   },
   redis: {
-    host: '8.134.132.121',
+    host: '127.0.0.1',
     port: 6379,
   },
   kueRedis: {
-    host: '8.134.132.121',
+    host: '127.0.0.1',
     port: 6379,
   },
   sourceMap: 'static/sourcemap',
